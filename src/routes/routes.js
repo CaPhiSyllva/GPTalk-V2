3afbe6960a7497886556f39238a72be597759514
Cauã Philip Silva <107730032+CaPhiSyllva@users.noreply.github.com>
2025-06-29 23:31:03 -0300

    Delete src directory

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import promptController from '../controllers/prompt-controller.js';
-
-const routes = express.Router();
-
-routes.post('/api/prompt', promptController.sendText);
-
-export default routes;
